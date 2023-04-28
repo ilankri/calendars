2a80ad98fcf70bcf8b0f47f3257603f71b012213
swrup <olivier.pierre@geneanet.org>
2023-04-28 16:02:17 +0200

    test for gregorian too

--- test/julian.ml
+++ test/julian.ml
@@ -11,11 +11,16 @@
 open OUnit
 open Calendars
 
-let febLength year : int =
+(* gregorian and julian calendar differ by their leap year rules *)
+let julian_feb_len year =
   if (if year < 0 then year + 1 else year) mod 4 = 0 then 29 else 28
 
-let monthLength : int array =
-  [| 31; 28; 31; 30; 31; 30; 31; 31; 30; 31; 30; 31 |]
+let gregorian_feb_len year =
+  let year = if year < 0 then year + 1 else year in
+  if year mod 4 = 0 && ((not (year mod 100 = 0)) || year mod 400 = 0) then 29
+  else 28
+
+let month_len = [| 31; 28; 31; 30; 31; 30; 31; 31; 30; 31; 30; 31 |]
 
 let assert_equal_dmy =
   assert_equal ~printer:(fun { day; month; year; _ } ->
@@ -23,29 +28,46 @@ let assert_equal_dmy =
 
 let assert_equal_sdn = assert_equal ~printer:string_of_int
 
+let kind_to_string : type a. a kind -> string =
+ fun kind ->
+  match kind with
+  | Gregorian -> "Gregorian"
+  | Julian -> "Julian"
+  | French -> "French"
+  | Hebrew -> "Hebrew"
+
+let test : type a. a kind -> (int -> a date) -> (int -> int) -> int -> test =
+ fun kind of_sdn feb_len sdn_offset ->
+  Printf.sprintf "%s <-> SDN" (kind_to_string kind) >:: fun _ ->
+  (* we start the loop on 1 january -4713 (SDN=0); but this does not correspond to the same SDN
+     for Julian and Gregorian this is why we have a +38 offset for Gregorian calendar *)
+  let sdn = ref sdn_offset in
+  for year = -4713 to 10000 do
+    (* year zero does not exists *)
+    if year <> 0 then
+      for month = 1 to 12 do
+        for
+          day = 1
+          to if month = 2 then feb_len year
+             else Array.get month_len @@ (month - 1)
+        do
+          let d =
+            match make kind ~day ~month ~year ~delta:0 with
+            | Ok d -> d
+            | Error s -> failwith s
+          in
+          let sdn' = to_sdn d in
+          assert_equal_sdn !sdn sdn';
+          assert_equal_dmy d (of_sdn sdn');
+          incr sdn
+        done
+      done
+  done
+
 let _ =
   run_test_tt_main
-    begin
-      "Julian <-> SDN" >:: fun _ ->
-      let sdn = ref 0 in
-      for year = -4713 to 10000 do
-        if year <> 0 then
-          for month = 1 to 12 do
-            for
-              day = 1
-              to if month = 2 then febLength year
-                 else Array.get monthLength @@ (month - 1)
-            do
-              let d =
-                match make Julian ~day ~month ~year ~delta:0 with
-                | Ok d -> d
-                | Error s -> failwith s
-              in
-              let sdn' = to_sdn d in
-              assert_equal_sdn !sdn sdn';
-              assert_equal_dmy d (julian_of_sdn sdn');
-              incr sdn
-            done
-          done
-      done
-    end
+    ("test suite for Calendars"
+    >::: [
+           test Julian julian_of_sdn julian_feb_len 0;
+           test Gregorian gregorian_of_sdn gregorian_feb_len 38;
+         ])
